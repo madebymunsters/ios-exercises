ccdd1d4daecb82f72d9175fa612f8f2373700dad
Joey Kirk <joey.kirk@gmail.com>
2015-07-09 11:06:49 -0400

    Assignment 14 completed

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,44 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    /* Double an integer */
+    NSInteger doubleNumber = [number integerValue] * 2;
+    return @(doubleNumber);
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    /* Find an array of numbers between high and low */
+    if (number == otherNumber) {
+        return @[@(number)];
+    }
+    
+    NSInteger lowNumber, highNumber;
+    
+    if (number < otherNumber) {
+        lowNumber = number;
+        highNumber = otherNumber;
+    } else {
+        lowNumber = otherNumber;
+        highNumber = number;
+    }
+    
+    NSMutableArray *returnArray = [NSMutableArray array];
+    
+    for (NSInteger i = lowNumber; i <= highNumber; i++) {
+        [returnArray addObject:@(i)];
+    }
+    
+    return returnArray;
+    
+    
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    /* Find lowest number in an array */
+    NSNumber *min=[arrayOfNumbers valueForKeyPath:@"@min.self"];
+    return [min intValue];
 }
 
 @end
